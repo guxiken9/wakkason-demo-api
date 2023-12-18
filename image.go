1ffbf386f1dc6183ad27373176aa25f14d1983b1
annkara <howtobecomeahacker@gmail.com>
2023-12-19 08:28:45 +0900

    Base64データの処理修正

--- image.go
+++ image.go
@@ -1,12 +1,20 @@
 package main
 
-import "encoding/base64"
+import (
+	"encoding/base64"
+	"errors"
+	"strings"
+)
 
 func DecodeBase64(text string) ([]byte, error) {
-	result, err := base64.StdEncoding.DecodeString(text)
-	if err != nil {
-		return nil, err
-	}
 
-	return result, err
+	t := strings.Split(text, ",")
+	if len(t) == 2 {
+		result, err := base64.StdEncoding.DecodeString(t[1])
+		if err != nil {
+			return nil, err
+		}
+		return result, nil
+	}
+	return nil, errors.New("不正なBase64データが指定されましたあ")
 }
